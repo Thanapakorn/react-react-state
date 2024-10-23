7aabe646e0692c4fdeb581cca9d6d9ed0adca1ca
Eisss20 <pajenan.j@gmail.com>
2024-10-23 18:51:32 +0700

    Feat: Add the answer assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
+import { useState } from "react";
+
 
 function App() {
+  const [message,setHelloMessage] = useState("Greeting Message");
+
+  const Sayhello = (text) => {
+  return setHelloMessage(text);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={()=>Sayhello("สวัสดี!")}> สวัสดี!</button>
+        <button onClick={()=>Sayhello("Hi!")}> Hi!</button>
+        <button onClick={()=>Sayhello("你好!")}> 你好!</button>
       </div>
     </div>
   );
