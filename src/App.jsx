f0680c2f89b5c89f914c6f1031c97c1b207fba91
thanapakorn <thanapakorn001@gmail.com>
2024-10-23 13:41:22 +0700

    feat:add answer ex react-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [mgs, setmsg] = useState("Greeting Message");
+
+  const handleMsg = (mgs) => {
+    setmsg(mgs);
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{mgs}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleMsg("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => handleMsg("Hi!")}>Hi!</button>
+        <button onClick={() => handleMsg("你好!")}>你好!</button>
       </div>
     </div>
   );
